20885237a7c2e8cb71a1343c297adb43d36e8b2b
vithushen <78235386+vithushen@users.noreply.github.com>
2022-03-30 13:24:18 -0400

    Update ArithmeticOperationsTest.java

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -17,7 +17,7 @@ public class ArithmeticOperationsTest {
 		assertEquals(expected, actual);
 
 	}
-   /* @Test
+  @Test
     public void testSub()
     {
 
@@ -27,5 +27,5 @@ public class ArithmeticOperationsTest {
         assertEquals(expected, actual);
 
     }
-*/
+
 }
